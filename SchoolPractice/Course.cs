3258f39eb423ced830a997820d12899f6bcffb12
Tilahun Demissie <til.assf98@gmail.com>
2021-09-15 09:50:08 -0500

    Exercise 4.7 updating adding features

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string CourseName { get; set; }
+        public Teacher instructor { get; set; }
+        public List<Student> enrolledStudents { get; set; }
+    }
+}
